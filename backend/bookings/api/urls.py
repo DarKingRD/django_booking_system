277f824e0fcad67ab709da41db7b168aed3f7f6c
DarKingRD <finedend@gmail.com>
2025-11-14 17:43:51 +0300

    create urls for api

---
+++ backend/bookings/api/urls.py
@@ -0,0 +1,10 @@
+"""Маршруты API для приложения bookings."""
+from rest_framework.routers import DefaultRouter
+from .views import RoomViewSet, BookingViewSet   
+
+router = DefaultRouter()
+
+router.register(r'rooms', RoomViewSet, basename='room')
+router.register(r'bookings', BookingViewSet, basename='booking')
+
+urlpatterns = router.urls
