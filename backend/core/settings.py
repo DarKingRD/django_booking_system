92b33b7864ff696430c5f80ce7cbd66d3bb2ea8f
DarKingRD <finedend@gmail.com>
2025-11-14 18:01:38 +0300

    fix error with rest_framework settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
