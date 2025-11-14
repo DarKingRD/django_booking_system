dc6e7ee2de714b315f88c7e67725897c681b7b84
DarKingRD <finedend@gmail.com>
2025-11-14 17:54:53 +0300

    add permission classes for RoomViewSet

--- backend/bookings/api/views.py
+++ backend/bookings/api/views.py
@@ -6,9 +6,9 @@
   с автоматической привязкой к авторизованному пользователю.
 """
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from bookings.models import Room, Booking
 from .serializers import RoomSerializer, BookingSerializer
-
 
 class RoomViewSet(viewsets.ReadOnlyModelViewSet):
     """Представление для просмотра переговорных комнат.
@@ -21,6 +21,7 @@ class RoomViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    permission_classes = [AllowAny]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
